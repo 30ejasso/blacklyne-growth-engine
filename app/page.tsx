69245ce7dea8cbbd3b60d192eeed788211fa60f5
Eduardo Jasso <30.ejasso@gmail.com>
2026-04-15 15:56:47 -0700

    Fix CapitalSection import to use default export

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CapitalSection } from "@/components/CapitalSection";
+import CapitalSection from "@/components/CapitalSection";
 
 export default function Home() {
   return (
